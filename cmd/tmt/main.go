52ab5469f3f8f47065c87c54cfbf5036584f4d8e
Max Kovalevskii <usrmaxoid@gmail.com>
2024-02-10 10:37:36 +0100

    Added functionality of deleting task in the CLI

--- cmd/tmt/main.go
+++ cmd/tmt/main.go
@@ -22,9 +22,14 @@ func main() {
 		fmt.Fprintln(output, "Usage information: ")
 		flag.PrintDefaults()
 	}
-	add := flag.Bool("add", false, "Add new task in the list")
-	list := flag.Bool("list", false, "List all tasks")
-	complete := flag.Int("complete", 0, "Task to be completed")
+	var addTask bool
+	var list bool
+	var completeTask int
+	var deleteTask int
+	flag.BoolVar(&addTask, "add", false, "Add new task in the list")
+	flag.BoolVar(&list, "list", false, "List all tasks")
+	flag.IntVar(&completeTask, "complete", 0, "Task to complete")
+	flag.IntVar(&deleteTask, "delete", 0, "Task to delete")
 	flag.Parse()
 
 	l := &tmt.List{}
@@ -34,32 +39,55 @@ func main() {
 	}
 
 	switch {
-	case *list:
-		// list current items to do
-		fmt.Print(l)
-	case *complete > 0:
-		if err := l.Complete(*complete); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-		if err := l.Save(tasksFileName); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-	case *add:
-		newTask, err := getTask(os.Stdin, flag.Args()...)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-		l.Add(newTask)
-		if err := l.Save(tasksFileName); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
+	case list:
+		handleShowList(l)
+	case completeTask > 0:
+		handleCompleteTask(completeTask, l)
+	case addTask:
+		handleAddTask(l)
+	case deleteTask > 0:
+		handleDeleteTask(deleteTask, l)
 	default:
 		fmt.Fprintln(os.Stderr, "Invalid option")
 		os.Exit(1)
+	}
+}
+
+func handleCompleteTask(id int, l *tmt.List) {
+	if err := l.Complete(id); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	if err := l.Save(tasksFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func handleShowList(l *tmt.List) {
+	// list current items to do
+	fmt.Print(l)
+}
+
+func handleAddTask(l *tmt.List) {
+	newTask, err := getTask(os.Stdin, flag.Args()...)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	l.Add(newTask)
+	if err := l.Save(tasksFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func handleDeleteTask(id int, l *tmt.List) {
+	if err := l.Delete(id); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	if err := l.Save(tasksFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
 	}
 }
 
